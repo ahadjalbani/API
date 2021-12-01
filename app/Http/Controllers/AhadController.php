72d89496a86a5fbabf2a0ea1f594deb543ca7906
ahadjalbani <ahadjalbani123@gmail.com>
2021-12-01 18:31:26 +0500

    API WORK

---
+++ app/Http/Controllers/AhadController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AhadController extends Controller
+{
+     public function getData(){
+
+        return ["name"=>"ahad","email"=>"ahad125@example","address"=>"dha karachi "];
+
+     } 
+}
