685a1efeb9eaec84a3e4ec479bdb262c7f60dc42
ahadjalbani <ahadjalbani123@gmail.com>
2021-12-03 18:30:37 +0500

    Api part 2

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AhadController;
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +23,37 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('data',[AhadController::class,'getData']);
+// Route::get('data',[AhadController::class,'getData']);
 
-Route::get('list/{id?}',[DeviceController::class,'list']);
+// Route::get('list',[DeviceController::class,'list']);
+
+// Route::post("add",[DeviceController::class,'add']);
+
+// Route::put("update",[DeviceController::class,'update']);
+
+// Route::delete("delete/{id}",[DeviceController::class,'delete']);
+
+// Route::post("save",[DeviceController::class,'testdata']);
+
+Route::post('/register',[AuthController::class,'register']);
+
+Route::post('/login',[AuthController::class,'login']);
+
+Route::group(['middleware'=>['auth:sanctum']],function(){
+
+Route::get('/posts',[PostController::class,'index']);
+
+Route::post('/post',[PostController::class,'store']);
+
+Route::get('/posts/{id}',[PostController::class,'show']);
+
+Route::put('/posts/{id}',[PostController::class,'update']);
+
+Route::delete('/posts/{id}',[PostController::class,'destroy']);
+
+Route::post('/logout',[AuthController::class,'logout']);
+
+});
+
+
 
